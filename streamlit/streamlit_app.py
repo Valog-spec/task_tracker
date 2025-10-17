e60888a43f410a109020141a6cb16ccbbc3cc4f1
Виктор Сагалаков <sagalakov.viktor@gmail.com>
2025-10-17 18:08:04 +0700

    fixed project

--- streamlit/streamlit_app.py
+++ streamlit/streamlit_app.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_BASE_URL = os.getenv("API_BASE_URL", "http://localhost:8080")
+API_BASE_URL = os.getenv("API_BASE_URL", "http://localhost:8080/api")
 st.set_page_config(page_title="JWT  Test")
 
 
